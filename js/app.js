6e8920417960d4e182eacbcc9ad93de0d7a2aa81
Roman Dulman <romandulman@gmail.com>
2019-05-04 21:41:37 +0300

    ok

--- js/app.js
+++ js/app.js
@@ -3,19 +3,23 @@ let flag = 0, i = 0;
 $(document).ready(function () {
     localStorage.clear();
 
+    $("#homeBtn").click(function () {
+        $('#outReports').hide();
+        $('#out').html('');
+        printCoins("https://api.coingecko.com/api/v3/coins/list");
+    });
+
     $("#liveRepBtn").click(function () {
         $('#out').html('');
-        $('#out').append(
+        $('#outReports').show();
 
-
-        )
 
     });
     $("#aboutBtn").click(function () {
+        $('#outReports').hide();
         $('#out').html('');
         $('#out').append(
 `<div class="col-md-4"></div>
-
 
 <div class="col-md-4"><h1>About</h1>
 <p></p>
@@ -39,12 +43,12 @@ $(document).ready(function () {
     $("#srchBtn").click(function () {
         $("#out").html('');
         url = "https://api.coingecko.com/api/v3/coins/" + $("#srchFld").val();
-        printCountry(url)
+        printCoins(url)
     });
-    printCountry("https://api.coingecko.com/api/v3/coins/list");
+    printCoins("https://api.coingecko.com/api/v3/coins/list");
 });
 
-let printCountry = (url) => {
+let printCoins = (url) => {
     $.ajax({
         url: url,
         type: "GET",
@@ -57,7 +61,11 @@ let printCountry = (url) => {
                 response.forEach(function (element) {
 
                     $('#out').append(
-                        `        <div class="card col-md-3 cardCss">
+
+                        `  
+                    <div class=" col-lg-3 col-md-6 ">
+
+        <div class="card shadow p-3 mb-5 cardCss">
             <div class="row no-gutters">
 
                 <div class="col">
@@ -67,7 +75,8 @@ let printCountry = (url) => {
                           <div class="collapse" id="N${element.id}"></div>
                                                   <button class="btn btn-info moreBtn" id="${element.id}" onclick="collapseFunc(this.id)" >More Info</button>
 
-             </div> `
+             </div></div>
+              `
                     )
 
                   ;
@@ -75,17 +84,19 @@ let printCountry = (url) => {
                 });
             }else{
                 $('#out').append(
-                    `        <div class="card col-md-3 cardCss">
+                    `         <div class=" col-lg-3 col-md-6 SingleColCss ">
+
+        <div class="card shadow p-3 mb-5 ">
             <div class="row no-gutters">
 
                 <div class="col">
                     <div class="card-block px-2">
                         <h4 class="card-title" id="symOut">${response.symbol}</h4><label class="switch"><input type="checkbox" id="${response.symbol}" onchange="checkCoinCount(this.id)" />    <div></div></label>
                         <p class="card-text" ><span id="nameOut">${response.name}</span></p>
-                          <div class="collapse" id="${response.id}"></div>
+                          <div class="collapse" id="N${response.id}"></div>
                                                   <button class="btn btn-info moreBtn" id="${response.id}" onclick="collapseFunc(this.id)" >More Info</button>
 
-             </div> `
+             </div> </div>`
                 )
             }
 
@@ -163,26 +174,144 @@ let updateChart=()=>{
         type: "GET",
 
         success: function (response) {
-               //  response.forEach(function (element) {
-               //
-               //  //    $('#out').append(
-               //  //        `
-               //
-               //    //      `
-               //  //    )
-               //      alert(element[0])
-               //
-               // });
-            // let r = response.ZRX.USD
-let c = "ZCN"
-            alert(response[c].USD)
-            // var string1 = "";
-            // var object1 = response;
-            //
-            // for (var property1 in object1) {
-            //    // string1 += ;
-            //     console.log(object1["ZCN"]);
-            // }
+
+
+            var dataPoints1 = [];
+            var dataPoints2 = [];
+            var dataPoints3 = [];
+
+            var options = {
+                title: {
+                    text: "Electricity Generation in Turbine"
+                },
+                axisX: {
+                    title: "chart updates every 2 secs"
+                },
+                axisY: {
+                    suffix: "Wh",
+                    includeZero: false
+                },
+                toolTip: {
+                    shared: true
+                },
+                legend: {
+                    cursor: "pointer",
+                    verticalAlign: "top",
+                    fontSize: 22,
+                    fontColor: "dimGrey",
+                    itemclick: toggleDataSeries
+                },
+                data: [{
+                    type: "line",
+                    xValueType: "dateTime",
+                    yValueFormatString: "###.00Wh",
+                    xValueFormatString: "hh:mm:ss TT",
+                    showInLegend: true,
+                    name: "Turbine 1",
+                    dataPoints: dataPoints1
+                },
+                    {
+                        type: "line",
+                        xValueType: "dateTime",
+                        yValueFormatString: "###.00Wh",
+                        showInLegend: true,
+                        name: "Turbine 2",
+                        dataPoints: dataPoints2
+                    }, {
+                        type: "line",
+                        xValueType: "dateTime",
+                        yValueFormatString: "###.00Wh",
+                        showInLegend: true,
+                        name: "Turbine 2",
+                        dataPoints: dataPoints3
+                    }]
+            };
+
+            var chart = $("#chartContainer").CanvasJSChart(options);
+
+            function toggleDataSeries(e) {
+                if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                }
+                else {
+                    e.dataSeries.visible = true;
+                }
+                e.chart.render();
+            }
+
+            var updateInterval = 2000;
+// initial value
+            var yValue1 = 800;
+            var yValue2 = 810;
+            var yValue3 = 780;
+
+            var time = new Date;
+// starting at 10.00 am
+            time.setHours(10);
+            time.setMinutes(00);
+            time.setSeconds(00);
+            time.setMilliseconds(00);
+
+            function updateChart(count) {
+                count = count || 1;
+                var deltaY1, deltaY2, deltaY3;
+                for (var i = 0; i < count; i++) {
+                    time.setTime(time.getTime() + updateInterval);
+                    deltaY1 = -1 + Math.random() * (1 + 1);
+                    deltaY2 = -1 + Math.random() * (1 + 1);
+                    deltaY3 = -1 + Math.random() * (1 + 1);
+
+                    // adding random value and rounding it to two digits.
+                    yValue1 = Math.round((yValue1 + deltaY1) * 100) / 100;
+                    yValue2 = Math.round((yValue2 + deltaY2) * 100) / 100;
+                    yValue3 = Math.round((yValue3 + deltaY3) * 100) / 100;
+
+                    // pushing the new values
+                    dataPoints1.push({
+                        x: time.getTime(),
+                        y: yValue1
+                    });
+                    dataPoints2.push({
+                        x: time.getTime(),
+                        y: yValue2
+                    });
+                    dataPoints3.push({
+                        x: time.getTime(),
+                        y: yValue3
+                    });
+                }
+
+                // updating legend text with  updated with y Value
+                options.data[0].legendText = "Turbine 1 : " + yValue1 + "Wh";
+                options.data[1].legendText = "Turbine 2 : " + yValue2 + "Wh";
+                options.data[2].legendText = "Turbine 3 : " + yValue3 + "Wh";
+                $("#chartContainer").CanvasJSChart().render();
+            }
+// generates first set of dataPoints
+            updateChart(100);
+            setInterval(function () { updateChart() }, updateInterval);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    $('#outReports').append(
+       //           `
+         //      `
+       //      )
+
+
+         //   alert(response[conisToView[0]].USD)
+         //   alert(response[conisToView[1]].USD)
 
 
         },
@@ -203,7 +332,7 @@ let c = "ZCN"
 
 
 
-}
+};
 
 
 let collapseFunc = (coinId) => {
